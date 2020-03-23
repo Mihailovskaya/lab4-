c01232d34d77c00aa36038abd7ea3552580ba52b
Mihailovskaya <sofya.mihailovskaya@yandex.ru>
2020-03-23 12:19:53 +0300

    Начальный возврат модуля lab4web

---
+++ lab4web/Program.cs
@@ -0,0 +1,472 @@
+﻿using System;
+
+namespace lab4web
+{
+    /// <summary>
+    /// класс Google
+    /// </summary>
+    public class Google : WebManagementSystem
+    {
+        /// <summary>
+        /// Сервис поиска для класса Google
+        /// </summary>
+        public void Search(/*входные данные*/)
+        {
+            Console.WriteLine("вызван метод Search для класса Google");
+        }
+        /// <summary>
+        /// Сервис карты для класса Google
+        /// </summary>
+        public void Maps(/*входные данные*/)
+        {
+            Console.WriteLine("вызван метод Maps для класса Google");
+        }
+        /// <summary>
+        /// Сервис фото для класса Google
+        /// </summary>
+        public void Photo(/*входные данные*/)
+        {
+            Console.WriteLine("вызван метод Photo для класса Google");
+        }
+        /// <summary>
+        /// Сервис переводчика для класса Google
+        /// </summary>
+        public void Interpreter(/*входные данные*/)
+        {
+            Console.WriteLine("вызван метод Interpreter для класса Google");
+        }
+        /// <summary>
+        /// Сервис календарь для класса Google
+        /// </summary>
+        public void Calendar(/*входные данные*/)
+        {
+            Console.WriteLine("вызван метод Calendar для класса Google");
+        }
+        /// <summary>
+        /// Сервис облако для класса Google
+        /// </summary>
+        public void Cloud(/*входные данные*/)
+        {
+            Console.WriteLine("вызван метод Cloud для класса Google");
+        }
+        /// <summary>
+        /// Сервис видео для класса Google
+        /// </summary>
+        public void Video(/*входные данные*/)
+        {
+            Console.WriteLine("вызван метод Video для класса Google");
+        }
+        /// <summary>
+        /// Сервис почты для класса Google
+        /// </summary>
+        public void Mail(/*входные данные*/)
+        {
+            Console.WriteLine("вызван метод Mail для класса Google");
+        }
+        /// <summary>
+        /// Сервис новостей для класса Google
+        /// </summary>
+        public void News(/*входные данные*/)
+        {
+            Console.WriteLine("вызван метод News для класса Google");
+        }
+        /// <summary>
+        /// Сервис погоды для класса Google
+        /// </summary>
+        public void Weather(/*входные данные*/)
+        {
+            Console.WriteLine("вызван метод Weather для класса Google");
+        }
+    }
+    /// <summary>
+    /// класс Yandex
+    /// </summary>
+    public class Yandex : WebManagementSystem
+    {
+        /// <summary>
+        /// Сервис поиска для класса Yandex
+        /// </summary>
+        public void Search(/*входные данные*/)
+        {
+            Console.WriteLine("вызван метод Search для класса Yandex");
+        }
+        /// <summary>
+        /// Сервис карты для класса Yandex
+        /// </summary>
+        public void Maps(/*входные данные*/)
+        {
+            Console.WriteLine("вызван метод Maps для класса Yandex");
+        }
+        /// <summary>
+        /// Сервис фото для класса Yandex
+        /// </summary>
+        public void Photo(/*входные данные*/)
+        {
+            Console.WriteLine("вызван метод Photo для класса Yandex");
+        }
+        /// <summary>
+        /// Сервис переводчик для класса Yandex
+        /// </summary>
+        public void Interpreter(/*входные данные*/)
+        {
+            Console.WriteLine("вызван метод Interpreter для класса Yandex");
+        }
+        /// <summary>
+        /// Сервис календарь для класса Yandex
+        /// </summary>
+        public void Calendar(/*входные данные*/)
+        {
+            Console.WriteLine("вызван метод Calendar для класса Yandex");
+        }
+        /// <summary>
+        /// Сервис облако для класса Yandex
+        /// </summary>
+        public void Cloud(/*входные данные*/)
+        {
+            Console.WriteLine("вызван метод Cloud для класса Yandex");
+        }
+        /// <summary>
+        /// Сервис видео для класса Yandex
+        /// </summary>
+        public void Video(/*входные данные*/)
+        {
+            Console.WriteLine("вызван метод Video для класса Yandex");
+        }
+        /// <summary>
+        /// Сервис почты для класса Yandex
+        /// </summary>
+        public void Mail(/*входные данные*/)
+        {
+            Console.WriteLine("вызван метод Mail для класса Yandex");
+        }
+        /// <summary>
+        /// Сервис новости для класса Yandex
+        /// </summary>
+        public void News(/*входные данные*/)
+        {
+            Console.WriteLine("вызван метод News для класса Yandex");
+        }
+        /// <summary>
+        /// Сервис погода для класса Yandex
+        /// </summary>
+        public void Weather(/*входные данные*/)
+        {
+            Console.WriteLine("вызван метод Weather для класса Yandex");
+        }
+    }
+    /// <summary>
+    /// класс Baidu
+    /// </summary>
+    public class Baidu : WebManagementSystem
+    {
+        /// <summary>
+        /// Сервис поиска для класса Baidu
+        /// </summary>
+        public void Search(/*входные данные*/)
+        {
+            Console.WriteLine("вызван метод Search для класса Baidu");
+        }
+        /// <summary>
+        /// Сервис карты для класса Baidu
+        /// </summary>
+        public void Maps(/*входные данные*/)
+        {
+            Console.WriteLine("вызван метод Maps для класса Baidu");
+        }
+        /// <summary>
+        /// Сервис фото для класса Baidu
+        /// </summary>
+        public void Photo(/*входные данные*/)
+        {
+            Console.WriteLine("вызван метод Photo для класса Baidu");
+        }
+        /// <summary>
+        /// Сервис переводчик для класса Baidu
+        /// </summary>
+        public void Interpreter(/*входные данные*/)
+        {
+            Console.WriteLine("вызван метод Interpreter для класса Baidu");
+        }
+        /// <summary>
+        /// Сервис календарь для класса Baidu
+        /// </summary>
+        public void Calendar(/*входные данные*/)
+        {
+            Console.WriteLine("вызван метод Calendar для класса Baidu");
+        }
+        /// <summary>
+        /// Сервис облако для класса Baidu
+        /// </summary>
+        public void Cloud(/*входные данные*/)
+        {
+            Console.WriteLine("вызван метод Cloud для класса Baidu");
+        }
+        /// <summary>
+        /// Сервис видео для класса Baidu
+        /// </summary>
+        public void Video(/*входные данные*/)
+        {
+            Console.WriteLine("вызван метод Video для класса Baidu");
+        }
+        /// <summary>
+        /// Сервис почты для класса Baidu
+        /// </summary>
+        public void Mail(/*входные данные*/)
+        {
+            Console.WriteLine("вызван метод Mail для класса Baidu");
+        }
+        /// <summary>
+        /// Сервис новости для класса Baidu
+        /// </summary>
+        public void News(/*входные данные*/)
+        {
+            Console.WriteLine("вызван метод News для класса Baidu");
+        }
+        /// <summary>
+        /// Сервис погоды для класса Baidu
+        /// </summary>
+        public void Weather(/*входные данные*/)
+        {
+            Console.WriteLine("вызван метод Weather для класса Baidu");
+        }
+    }
+
+    /// <summary>
+    /// интерфейс, предоставляет методы, реализующиеся для любой компании
+    /// </summary>
+    public interface WebManagementSystem
+    {
+        /// <summary>
+        /// сервис посиска
+        /// </summary>
+        void Search(/*входные данные*/);
+        /// <summary>
+        /// сервис карт
+        /// </summary>
+        void Maps(/*входные данные*/);
+        /// <summary>
+        /// сервис фото
+        /// </summary>
+        void Photo(/*входные данные*/);
+        /// <summary>
+        /// сервис переводчик
+        /// </summary>
+        void Interpreter(/*входные данные*/);
+        /// <summary>
+        /// сервис календарь
+        /// </summary>
+        void Calendar(/*входные данные*/);
+        /// <summary>
+        /// сервис облако
+        /// </summary>
+        void Cloud(/*входные данные*/);
+        /// <summary>
+        /// сервис видео
+        /// </summary>
+        void Video(/*входные данные*/);
+        /// <summary>
+        /// сервис почты
+        /// </summary>
+        void Mail(/*входные данные*/);
+        /// <summary>
+        /// сервис новости
+        /// </summary>
+        void News(/*входные данные*/);
+        /// <summary>
+        /// сервис погода
+        /// </summary>
+        void Weather(/*входные данные*/);
+    }
+    /// <summary>
+    /// класс, отвечающий за отправку запроса
+    /// </summary>
+    public class View
+    {
+        
+        private readonly WebManagementSystem wb;
+        /// <summary>
+        /// конструктор
+        /// </summary>
+        /// <param name="wb"></param>
+        public View(WebManagementSystem wb)
+        {
+            this.wb = wb;
+        }
+        /// <summary>
+        /// Метод, позволяющий получить доступ к методу Search
+        /// </summary>
+        public void Search(/*входные данные*/)
+        {
+            wb.Search(/*входные данные*/);
+        }
+        /// <summary>
+        /// Метод, позволяющий получить доступ к методу Maps
+        /// </summary>
+        public void Maps(/*входные данные*/)
+        {
+            wb.Maps(/*входные данные*/);
+        }
+        /// <summary>
+        /// Метод, позволяющий получить доступ к методу Photo
+        /// </summary>
+        public void Photo(/*входные данные*/)
+        {
+            wb.Photo(/*входные данные*/);
+        }
+        /// <summary>
+        /// Метод, позволяющий получить доступ к методу Interpreter
+        /// </summary>
+        public void Interpreter(/*входные данные*/)
+        {
+            wb.Interpreter(/*входные данные*/);
+        }
+        /// <summary>
+        /// Метод, позволяющий получить доступ к методу Calendar
+        /// </summary>
+        public void Calendar(/*входные данные*/)
+        {
+            wb.Calendar(/*входные данные*/);
+        }
+        /// <summary>
+        /// Метод, позволяющий получить доступ к методу Cloud
+        /// </summary>
+        public void Cloud(/*входные данные*/)
+        {
+            wb.Cloud(/*входные данные*/);
+        }
+        /// <summary>
+        /// Метод, позволяющий получить доступ к методу Video
+        /// </summary>
+        public void Video(/*входные данные*/)
+        {
+            wb.Video(/*входные данные*/);
+        }
+        /// <summary>
+        /// Метод, позволяющий получить доступ к методу Mail
+        /// </summary>
+        public void Mail(/*входные данные*/)
+        {
+            wb.Mail(/*входные данные*/);
+        }
+        /// <summary>
+        /// Метод, позволяющий получить доступ к методу News
+        /// </summary>
+        public void News(/*входные данные*/)
+        {
+            wb.News(/*входные данные*/);
+        }
+        /// <summary>
+        /// Метод, позволяющий получить доступ к методу Weather
+        /// </summary>
+        public void Weather(/*входные данные*/)
+        {
+            wb.Weather(/*входные данные*/);
+        }
+
+    }
+
+    /// <summary>
+    /// класс Program
+    /// </summary>
+    class Program
+    {
+        public static View view;
+        /// <summary>
+        /// обрабатывает полученные данные
+        /// </summary>
+        /// <param name="company">выбор компании</param>
+        /// <param name="service">выбор сервиса</param>
+        private static void Request(string company, string service)
+        {
+            switch (company) /*В зависимости от числа определяется, к какой СУБД нужен доступ.*/
+            {
+                case "1":
+                    Google google = new Google();
+                    view = new View(google);
+                    break;
+                case "2":
+                    Yandex yandex = new Yandex();
+                    view = new View(yandex);
+                    break;
+                case "3":
+                    Baidu baidu = new Baidu();
+                    view = new View(baidu);
+                    break;
+                default:
+                    throw new Exception("Ошибка при выборе компании");
+            }
+            switch (service) /*В зависимости от числа определяется, к какой команде СУБД нужен доступ.*/
+            {
+                case "1":
+                    view.Search();
+                    break;
+                case "2":
+                    view.Maps();
+                    break;
+                case "3":
+                    view.Photo();
+                    break;
+                case "4":
+                    view.Interpreter();
+                    break;
+                case "5":
+                    view.Calendar();
+                    break;
+                case "6":
+                    view.Cloud();
+                    break;
+                case "7":
+                    view.Video();
+                    break;
+                case "8":
+                    view.Mail();
+                    break;
+                case "9":
+                    view.News();
+                    break;
+                case "10":
+                    view.Weather();
+                    break;
+                default:
+                    Console.WriteLine("Такой команды не существует.");
+                    break;
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                string company;
+                string service;
+               
+                Console.WriteLine("Введите номер выбранной компании:\r\n" +
+                                    "1. Google\r\n" +
+                                    "2. Yandex\r\n" +
+                                    "3. Baidu");
+                company = Console.ReadLine();
+                Console.WriteLine("Выберете сервис:\r\n" +
+                                    "1. Поиск\r\n" +
+                                    "2. Карты\r\n" +
+                                    "3. Фото\r\n" +
+                                    "4. Переводчик\r\n" +
+                                    "5. Календарь\r\n" +
+                                    "6. Облако\r\n" +
+                                    "7. Видео\r\n" +
+                                    "8. Почта\r\n" +
+                                    "9. Новости\r\n" +
+                                    "10. Погода");
+                service = Console.ReadLine();
+                Request(company, service);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+
+       
+        }
+    }
+}
